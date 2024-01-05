f13cb2ab0743737ef338488af180a67d3e179cf8
nyyakko <nyakonyns@gmail.com>
2024-01-04 22:52:47 -0300

    tooling: updated some things

--- configure.py
+++ configure.py
@@ -2,9 +2,5 @@ import os
 import sys
 
 BUILD_TYPE = sys.argv[1] if len(sys.argv) >= 2 else 'debug'
-TOOLCHAIN  = os.getenv('CMAKE_TOOLCHAIN_FILE')
 
-if not os.path.exists('build'):
-    os.mkdir('build')
-
-os.system(f'cmake -DWSL_DISABLE_TESTS=FALSE -DCMAKE_BUILD_TYPE={BUILD_TYPE} -DCMAKE_TOOLCHAIN_FILE={TOOLCHAIN} -G Ninja -D CMAKE_C_COMPILER=clang -D CMAKE_CXX_COMPILER=clang++ -B./build/ -S./')
+os.system(f'cmake -DCMAKE_BUILD_TYPE={BUILD_TYPE} -G Ninja -B./build/ -S./')
